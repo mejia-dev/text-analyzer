3357e08f02c3dbdd77cfa4a04de4d279c0dc3aa1
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-24 10:54:19 -0700

    Define list of offensive words. If array element contains offensive word, do not increase word count
    Co-authored-by: Nikkita Torres <nikkitatorres@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 // Business Logic
 
 function wordCounter(text) {
+  let offensiveWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
   if (text.trim().length === 0) {
     return 0;
   }
   let wordCount = 0;
   const textArray = text.split(" ");
-  textArray.forEach(function(element) {
-    if (!Number(element)) {
+  textArray.forEach(function (element) {
+    if (!Number(element) && !offensiveWords.includes(element)) {
       wordCount++;
     }
   });
@@ -18,7 +19,7 @@ function wordCounter(text) {
 function numberOfOccurrencesInText(word, text) {
   const textArray = text.split(" ");
   let wordCount = 0;
-  textArray.forEach(function(element) {
+  textArray.forEach(function (element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
       wordCount++;
     }
